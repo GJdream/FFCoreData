a888270f241e0ff8eef57cc976bebc062a99edbe
Cory D. Wiles <kwylez@gmail.com>
2013-01-30 21:55:54 -0500

    removed boilerplate core data code

--- FFCoreData/CWMasterViewController.m
+++ FFCoreData/CWMasterViewController.m
@@ -44,27 +44,27 @@
   [super didReceiveMemoryWarning];
 }
 
-- (void)insertNewObject:(id)sender {
-
-  NSManagedObjectContext *context   = [self.fetchedResultsController managedObjectContext];
-  NSEntityDescription *entity       = [[self.fetchedResultsController fetchRequest] entity];
-  NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name]
-                                                                    inManagedObjectContext:context];
-    
-  // If appropriate, configure the new managed object.
-  // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-  [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
-  
-  // Save the context.
-  NSError *error = nil;
-
-  if (![context save:&error]) {
-       // Replace this implementation with code to handle the error appropriately.
-       // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-      NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-      abort();
-  }
-}
+//- (void)insertNewObject:(id)sender {
+//
+//  NSManagedObjectContext *context   = [self.fetchedResultsController managedObjectContext];
+//  NSEntityDescription *entity       = [[self.fetchedResultsController fetchRequest] entity];
+//  NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name]
+//                                                                    inManagedObjectContext:context];
+//    
+//  // If appropriate, configure the new managed object.
+//  // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
+//  [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
+//  
+//  // Save the context.
+//  NSError *error = nil;
+//
+//  if (![context save:&error]) {
+//       // Replace this implementation with code to handle the error appropriately.
+//       // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+//      NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+//      abort();
+//  }
+//}
 
 #pragma mark - Table View
 
@@ -96,35 +96,6 @@
   [self configureCell:cell atIndexPath:indexPath];
 
   return cell;
-}
-
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-  // Return NO if you do not want the specified item to be editable.
-  return YES;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-
-  if (editingStyle == UITableViewCellEditingStyleDelete) {
-
-    NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
-
-    [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
-      
-    NSError *error = nil;
-
-    if (![context save:&error]) {
-      // Replace this implementation with code to handle the error appropriately.
-      // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-      NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-      abort();
-    }
-  }   
-}
-
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-  // The table view should not be re-orderable.
-  return NO;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
